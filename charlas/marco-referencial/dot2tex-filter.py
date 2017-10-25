aa9410e1ff6cd161e0d6b47b7797539623b751a5
lifofernandez <lifofernandez@gmail.com>
2017-10-25 18:58:15 -0300

    intro y graficos bien

--- charlas/marco-referencial/dot2tex-filter.py
+++ charlas/marco-referencial/dot2tex-filter.py
@@ -10,7 +10,7 @@ def dot2tex(key, value, format, meta):
     if key=="CodeBlock"and "dot" in value[0][1]:
         caption = ""
         label = ""
-        scale = "1"
+        scale = "2"
         for pair in value[0][2]:
             if pair[0] == "caption":
                 caption = pair[1]
@@ -19,12 +19,24 @@ def dot2tex(key, value, format, meta):
             if pair[0] == "scale":
                 scale = pair[1]
         graph = value[1]
-        p = Popen(['dot2tex', '--figonly', '--autosize', '--codeonly'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        p = Popen(['dot2tex', 
+            '--figonly', 
+            '--autosize', 
+            '--codeonly',
+            '--nominsize',
+            '-ftikz',
+            '--tikzedgelebels',
+            '--graphstyle=automaton',
+            '--crop',
+            '--nominsize',
+            '--autosize',
+            ], stdout=PIPE, stdin=PIPE, stderr=PIPE)
         response = p.communicate(input=bytes(graph, "utf-8"))
         err = response[1].decode("utf-8")
         if len(err) != 0:
             raise ValueError(err)
         tikz_code = response[0].decode("utf-8")
+
         result = "\\begin{figure}\n\\centering\\begin{tikzpicture}[scale=" + scale + "]" + tikz_code + "\\end{tikzpicture}\\caption{" + caption + "} \\label{" + label + "}\\end{figure}"
         return {
             "c": [
